3e4459d2f2339e37c58906a1a4b2cb556120da77
Dagoberto528 <dagoberto528@hotmail.com>
2017-03-16 18:28:35 -0500

    Commit 1
    Creacion de la pagina en asp.net

---
+++ Hola_Mundo_Application_Web/App/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/App/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Hola_Mundo_Application_Web.App.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Hola Mundo creado por:</h2>
+    <h3>Dagoberto Sabogal Cocoma</h3>
+
+</asp:Content>
---
+++ Hola_Mundo_Application_Web/App/Default.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Hola_Mundo_Application_Web.App {
+    
+    
+    public partial class Default {
+    }
+}
